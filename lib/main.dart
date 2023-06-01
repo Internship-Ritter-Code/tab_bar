b94d0ec5fd53a948f59db7cfcb3fa075e5cacef5
alamsyahh15 <muhamad.alamsyah@studioalva.co>
2023-06-01 13:47:14 +0700

    update tabbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 // ignore_for_file: unused_import
 
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:tab_bar/three.dart';
 import 'package:tab_bar/two.dart';
+
 import 'one.dart';
-import 'dart:convert';
 
-
-void main() { 
+void main() {
   runApp(const MyApp());
 }
 
@@ -20,27 +21,24 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Belajar Flutter',
-      theme: ThemeData(
-        primarySwatch: Colors.yellow
+      theme: ThemeData(primarySwatch: Colors.yellow),
+      home: BottomNavi(
+        title: '',
       ),
-      home: BottomNavi(title: '',),
     );
   }
 }
 
-
 class BottomNavi extends StatefulWidget {
-  BottomNavi ({Key? key, required this.title}) : super (key: key);
+  BottomNavi({Key? key, required this.title}) : super(key: key);
   final String title;
   @override
   _BottomNavi createState() => _BottomNavi();
 }
 
 class _BottomNavi extends State<BottomNavi> {
-
   int currentIndex = 0;
   final List<Widget> body = [
-
     Two(),
     One(),
     There(),
@@ -50,37 +48,41 @@ class _BottomNavi extends State<BottomNavi> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Hello', style: TextStyle(color: Colors.white),),
+        title: const Text(
+          'Hello',
+          style: TextStyle(color: Colors.white),
+        ),
       ),
       body: body[currentIndex],
       bottomNavigationBar: BottomNavigationBar(
-        // backgroundColor: Colors.blue,
+        unselectedItemColor: Colors.black,
+        selectedItemColor: Colors.red,
         onTap: ontap,
         currentIndex: currentIndex,
-        items: [
+        items: const [
           BottomNavigationBarItem(
-            icon: Icon(Icons.home, color: Colors.black45,),
+            icon: Icon(Icons.home),
             label: 'Home',
-            activeIcon: Icon(Icons.home, color: Colors.yellow,),
-            ),
-            BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle, color: Colors.black45,),
+            activeIcon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
             label: 'Account',
-            activeIcon: Icon(Icons.account_circle, color: Colors.yellow,),
-            ),
-            BottomNavigationBarItem(
-            icon: Icon(Icons.access_time, color: Colors.black45,),
+            activeIcon: Icon(Icons.account_circle),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.access_time),
             label: 'Time',
-            activeIcon: Icon(Icons.access_time, color: Colors.yellow,),
-            ),
+            activeIcon: Icon(Icons.access_time),
+          ),
         ],
-      ),   
+      ),
     );
   }
-  void ontap(int index){
+
+  void ontap(int index) {
     setState(() {
       currentIndex = index;
     });
   }
 }
-
