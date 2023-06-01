f6df04fe66b0a988da388e2ae92a0e3fb6d40fbd
alamsyahh15 <muhamad.alamsyah@studioalva.co>
2023-06-01 13:55:09 +0700

    statefull & stateless

--- lib/three.dart
+++ lib/three.dart
@@ -1,15 +1,42 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+// class There extends StatelessWidget {
+//   const There({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return InkWell(
+//       onTap: () {},
+//       child: Center(
+//         child: Text('three'),
+//       ),
+//     );
+//   }
+// }
 
 class There extends StatefulWidget {
   @override
   _threeState createState() => _threeState();
 }
 
-class _threeState  extends State<There> {
+class _threeState extends State<There> {
+  String variableSatu = "three";
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('three'),
+    return InkWell(
+      onTap: () {
+        print("Masuk sini");
+
+        // Merubah nilai variableSatu dari three menjadi dendy
+        variableSatu = "Dendy";
+
+        // Untuk render ulang / build ulang tampilan dengan tampilan terbaru
+        setState(() {});
+      },
+      child: Center(
+        child: Text(variableSatu),
+      ),
     );
   }
 }
